3c823287aeffb06405a0ee9da2344fe9eb97f34f
Alexkcshing <alexkcshing@gmail.com>
2019-01-21 12:35:57 +0000

    add test setup and initialise class

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -4,3 +4,8 @@ require_relative('../sports_team')
 
 
 class TestSportsTeam < MiniTest::Test
+
+
+def setup
+  @team_object = SportsTeam.new("Generic Team", ["Player 1"], ["Player 2"], ["Player 3"], "Mr Coach")
+end
---
+++ sports_team.rb
@@ -0,0 +1,10 @@
+class SportsTeam
+
+  def initialize(team_name, team_players, team_coach)
+    @team_name = team_name
+    @team_players = team_players
+    @team_coach = team_coach
+  end
+  
+
+end
