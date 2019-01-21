4a6f9226b9a7238cd524b92d376de4b65d509562
Alexkcshing <alexkcshing@gmail.com>
2019-01-21 12:40:43 +0000

    add getter tests

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -5,7 +5,20 @@ require_relative('../sports_team')
 
 class TestSportsTeam < MiniTest::Test
 
+  def setup
+    @team_object = SportsTeam.new("Generic Team", ["Player 1"], ["Player 2"], ["Player 3"], "Mr Coach")
+  end
 
-def setup
-  @team_object = SportsTeam.new("Generic Team", ["Player 1"], ["Player 2"], ["Player 3"], "Mr Coach")
+  def test_get_team_name
+    assert_equal("Generic Team", @team_object.get_team_name())
+  end
+
+  def test_get_team_players
+    assert_equal(["Player 1"], ["Player 2"], ["Player 3"], @team_object.get_team_players())
+  end
+
+  def test_get_team_coach
+    assert_equal("Mr Coach", @team_object.get_team_coach())
+  end
+
 end
