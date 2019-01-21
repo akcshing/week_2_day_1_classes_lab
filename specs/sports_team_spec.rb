4aaad2827df3684ffaaed4fdbb976c1ec58abb91
Alexkcshing <alexkcshing@gmail.com>
2019-01-21 12:46:08 +0000

    debug wrong number of arguments error

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -6,7 +6,7 @@ require_relative('../sports_team')
 class TestSportsTeam < MiniTest::Test
 
   def setup
-    @team_object = SportsTeam.new("Generic Team", ["Player 1"], ["Player 2"], ["Player 3"], "Mr Coach")
+    @team_object = SportsTeam.new("Generic Team", ["Player 1", "Player 2", "Player 3"], "Mr Coach")
   end
 
   def test_get_team_name
@@ -14,7 +14,7 @@ class TestSportsTeam < MiniTest::Test
   end
 
   def test_get_team_players
-    assert_equal(["Player 1"], ["Player 2"], ["Player 3"], @team_object.get_team_players())
+    assert_equal(["Player 1", "Player 2", "Player 3"], @team_object.get_team_players())
   end
 
   def test_get_team_coach
--- sports_team.rb
+++ sports_team.rb
@@ -5,6 +5,17 @@ class SportsTeam
     @team_players = team_players
     @team_coach = team_coach
   end
-  
+
+    def get_team_name
+      return @team_name
+    end
+
+    def get_team_players
+      return @team_players
+    end
+
+    def get_team_coach
+      return @team_coach
+    end
 
 end
