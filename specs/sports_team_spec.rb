dcceec81d561f258067f22f9879db0a6dda0bece
Alexkcshing <alexkcshing@gmail.com>
2019-01-21 13:14:09 +0000

    refactor getters to attr_reader and add attr_accessor for coach

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -10,15 +10,20 @@ class TestSportsTeam < MiniTest::Test
   end
 
   def test_get_team_name
-    assert_equal("Generic Team", @team_object.get_team_name())
+    assert_equal("Generic Team", @team_object.team_name())
   end
 
   def test_get_team_players
-    assert_equal(["Player 1", "Player 2", "Player 3"], @team_object.get_team_players())
+    assert_equal(["Player 1", "Player 2", "Player 3"], @team_object.team_players())
   end
 
   def test_get_team_coach
-    assert_equal("Mr Coach", @team_object.get_team_coach())
+    assert_equal("Mr Coach", @team_object.team_coach())
+  end
+
+  def test_set_coach_name
+    @team_object.team_coach = "Mr New Coach"
+    assert_equal("Mr New Coach", @team_object.team_coach())
   end
 
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,21 +1,26 @@
 class SportsTeam
 
+  attr_reader :team_name, :team_players, :team_coach
+  attr_writer :team_coach
+
   def initialize(team_name, team_players, team_coach)
     @team_name = team_name
     @team_players = team_players
     @team_coach = team_coach
   end
 
-    def get_team_name
-      return @team_name
-    end
+    # def get_team_name
+    #   return @team_name
+    # end
+    #
+    # def get_team_players
+    #   return @team_players
+    # end
+    #
+    # def get_team_coach
+    #   return @team_coach
+    # end
 
-    def get_team_players
-      return @team_players
-    end
 
-    def get_team_coach
-      return @team_coach
-    end
 
 end
