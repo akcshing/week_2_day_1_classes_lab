ef67344c25824234031327cf36fd4267ac039069
Alexkcshing <alexkcshing@gmail.com>
2019-01-21 12:04:48 +0000

    add test get cohort

--- specs/student_class_spec.rb
+++ specs/student_class_spec.rb
@@ -13,4 +13,9 @@ class TestStudent < MiniTest::Test
   def test_get_name
     assert_equal("Alex", @student.get_name())
   end
+
+  def test_get_cohort
+    assert_equal("E28", @student.get_cohort())
+  end
+  
 end
